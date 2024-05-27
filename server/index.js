ae200daade9ec1f4ea7b4237c091d261280033c8
user36 <user36@realtechltd.com>
2024-05-27 17:23:42 +0700

    upadte

--- server/index.js
+++ server/index.js
@@ -34,7 +34,6 @@ app.use((req, res, next) => {
   console.log(`Request received: ${req.method} ${req.url}`)
   next()
 })
-app.options('*', cors(corsOptions))
 app.use('/', router)
 
 connect(MONGODB_URI, {
