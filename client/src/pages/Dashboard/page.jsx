199c36b20bfcbc699c2bc3ecdca44b2d5c003c38
yhsdaniel <user36@realtechltd.com>
2024-12-04 14:50:28 +0700

    up

--- client/src/pages/Dashboard/page.jsx
+++ client/src/pages/Dashboard/page.jsx
@@ -1,19 +1,19 @@
 import Navbar from "../../components/Navbar";
 import Main from "./Main";
-import { motion, AnimatePresence } from 'framer-motion'
-import ListMovies from "../../components/ListMovies";
 import { title, apiMovies, typeFilms } from '../../lib/appConfig'
+
+const ListMovies = React.lazy(() => import('../../components/ListMovies'))
 
 export default function Homepage() {
   return (
     <>
       <Navbar />
       <Main />
-      <div>
+      <>
         <ListMovies title={title.popularMovies} slidingMovie={apiMovies.popularMovies} typesOfFilms={typeFilms.movie} />
         <ListMovies title={title.trendingTVSeries} slidingMovie={apiMovies.trendingTVSeries} typesOfFilms={typeFilms.tv} />
         <ListMovies title={title.topratedTVSeries} slidingMovie={apiMovies.topratedTVSeries} typesOfFilms={typeFilms.tv} />
-      </div>
+      </>
     </>
   )
 }
