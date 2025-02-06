7a849e72b6c31ca60d5bac44dfa584e00bb10d8a
terlanjurtampan36 <user36@realtechltd.com>
2025-02-06 15:05:30 +0700

    update

--- client/src/components/ui/Loading.jsx
+++ client/src/components/ui/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Loading() {
     return (
         <div className='container h-screen flex justify-center items-center overflow-hidden bg-black text-white'>
-            <div className='text-orange-500 text-4xl font-bold cursor-pointer'>NETEX</div>
+            <div className='text-orange-500 text-6xl font-bold cursor-pointer'>N</div>
         </div>
     )
 }
