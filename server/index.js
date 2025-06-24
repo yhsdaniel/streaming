dd32a5d63d87081f0c61d46ea7f29d455319ddb0
terlanjurtampan36 <user36@realtechltd.com>
2025-06-24 15:34:28 +0700

    coba

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
-import app from './app'
+// server/index.js
+import app from './app.js'
+import { createServer } from 'http'
 
+// ⛔️ DON'T use app(req, res) — Express doesn't support this format directly
+// ✅ Use Node's HTTP server interface
 export default function handler(req, res) {
-    return app(req, res)
+    const server = createServer(app)
+    return server.emit('request', req, res)
 }
